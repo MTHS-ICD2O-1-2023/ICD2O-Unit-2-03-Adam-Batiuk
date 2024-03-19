8a6f81dc6686c8c3b48fb7433fe464dd0b3de114
Adam-Batiuk <159185155+Adam-Batiuk@users.noreply.github.com>
2024-03-19 18:19:05 +0000

    done

--- js/script.js
+++ js/script.js
@@ -1,5 +1,15 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2024 Mr. Adam Batiuk All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Adam Batiuk
+// Created on: Mar 2024
 // This file contains the JS functions for index.html
+
+function enterClicked() {
+  // input
+  const streetName = document.getElementById("street-name").value
+  const streetNumber = parseInt(document.getElementById("street-number").value)
+
+  // output
+  document.getElementById("user-info").innerHTML =
+    "Your address is: " + streetNumber + ", " + streetName + "."
+}
